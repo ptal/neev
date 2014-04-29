b5b76601602b0effe29e85ab22d4d4f98e19643f
Pierre Talbot <ptalbot@hyc.io>
2014-04-29 07:48:14 +0200

    Add utility file to retreive an unique identifier of the client.

---
+++ example/chat/utility.hpp
@@ -0,0 +1,13 @@
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE.txt
+//
+// (C) Copyright 2014 Pierre Talbot <ptalbot@hyc.io>
+
+#include "boost/asio.hpp"
+
+std::string ip_port(const boost::shared_ptr<boost::asio::ip::tcp::socket>& socket)
+{
+  std::stringstream ip_port;
+  ip_port << socket->remote_endpoint();
+  return ip_port.str();
+}
